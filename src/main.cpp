0ae9c35207988c91d4509b42d328638a3bb7dd43
Patrick Kishino <patrick.a.kishino@gmail.com>
2018-04-06 09:50:01 +0200

    Simplified, removed screen and buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <SSD1306Wire.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
@@ -9,13 +7,9 @@
 #include <DHT.h>
 #include <PubSubClient.h>
 
-SSD1306Wire display(0x3c, 4, 5);
-
 #define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
-
-#define BUTTONPIN 12
 
 #define ssid "***REMOVED***"
 #define password "***REMOVED***"
@@ -32,100 +26,44 @@ WiFiClient wifiClient;
 String sensor = "sensor/";
 
 PubSubClient mqttClient(wifiClient);
-long lastMsg = 60000;
+long lastMsg = 0;
+long now = 600001;
 
 float humidity, temperature, heatIndex = 0.0;
 int raw, level = 0;
 
 void setupWifi();
 void setupOTA();
-void setupDisplay();
 void measureBattery();
 void measureEnvironment();
 void reconnectMqtt();
-void displayInterrupt();
-
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Serial up and running");
-    pinMode(BUTTONPIN, INPUT_PULLUP);
-    attachInterrupt(BUTTONPIN, displayInterrupt, RISING);
-    setupDisplay();
     setupWifi();
     setupOTA();
-    // mqttClient.setServer(mqtt_server, 1883);
-    Serial.println("Setup completed, turning display off");
-    display.clear();
-    // display.displayOff();
+    mqttClient.setServer(mqtt_server, 1883);
 }
 
 void loop()
 {
     ArduinoOTA.handle();
-    long now = millis();
-    display.clear();
     if (now - lastMsg > 1000 * 600)
     {
         Serial.println("Running measurements");
         lastMsg = now;
-        // reconnectMqtt();
+        reconnectMqtt();
         measureBattery();
         measureEnvironment();
     }
-}
-void setupDisplay()
-{
-    display.init();
-    display.flipScreenVertically();
-    display.setContrast(255);
-    display.setFont(ArialMT_Plain_10);
-    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-    display.clear();
+    now = millis();
 }
 void setupOTA()
 {
     const char *hostname = location.c_str();
     ArduinoOTA.setHostname(hostname);
     ArduinoOTA.setPassword(devicePass);
-
-    ArduinoOTA.onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-            type = "sketch";
-        else // U_SPIFFS
-            type = "filesystem";
-
-        display.clear();
-        display.drawString(display.getWidth() / 2, display.getHeight() / 2 - 10, "OTA Update");
-        display.display();
-    });
-    ArduinoOTA.onEnd([]() {
-        display.clear();
-        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Restart");
-        display.display();
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-
-        display.drawProgressBar(4, 32, 120, 8, progress / (total / 100));
-        display.display();
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-        display.clear();
-        display.drawString(display.getWidth() / 2, display.getHeight() / 2 - 10, "OTA Error :");
-        if (error == OTA_AUTH_ERROR)
-            display.drawString(display.getWidth() / 2, 32, "Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-            display.drawString(display.getWidth() / 2, 32, "Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-            display.drawString(display.getWidth() / 2, 32, "Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-            display.drawString(display.getWidth() / 2, 32, "Receive Failed");
-        else if (error == OTA_END_ERROR)
-            display.drawString(display.getWidth() / 2, 32, "End Failed");
-        display.display();
-        delay(5000);
-    });
     ArduinoOTA.begin();
 }
 void setupWifi()
@@ -136,13 +74,11 @@ void setupWifi()
     {
         location = "Bedroom";
         sensor += "bedroom";
-        // switch_number += '0';
     }
     else if (mac = "***REMOVED***")
     {
         location = "Living Room";
         sensor += "livingroom";
-        // switch_number += '1'
     }
     WiFi.persistent(false);
     WiFi.forceSleepWake();
@@ -151,24 +87,14 @@ void setupWifi()
     while (WiFi.waitForConnectResult() != WL_CONNECTED)
     {
         Serial.println("Connection Failed! Rebooting...");
-        display.drawString(display.getWidth() / 2, 10, "Connection Failed!");
-        display.display();
         delay(3000);
         ESP.restart();
     }
-
-    display.clear();
-    display.drawString(display.getWidth() / 2, display.getHeight() / 2 - 10, "IP:" + WiFi.localIP().toString());
-    display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Monitor for:" + location);
-    display.display();
-    delay(3000);
-    display.clear();
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    // display.displayOff();
+    Serial.println("Connected:" + WiFi.localIP().toString());
+    Serial.println("Location:" + location);
 }
 void reconnectMqtt()
 {
-
     while (!mqttClient.connected())
     {
         Serial.print("Connecting to MQTT broker ...");
@@ -191,8 +117,8 @@ void measureBattery()
     level = map(raw, 511, 718, 0, 100);
     Serial.println("Level:" + String(level) + '%');
     Serial.println("raw:" + String(raw));
-    // mqttClient.publish((sensor + "/battery_level").c_str(), String(level).c_str(), true);
-    // mqttClient.publish((sensor + "/battery_raw").c_str(), String(raw).c_str(), true);
+    mqttClient.publish((sensor + "/battery_level").c_str(), String(level).c_str(), true);
+    mqttClient.publish((sensor + "/battery_raw").c_str(), String(raw).c_str(), true);
 }
 void measureEnvironment()
 {
@@ -210,20 +136,7 @@ void measureEnvironment()
     Serial.print("Temperature: " + String(temperature) + "˚C\t");
     Serial.print("Heat Index:" + String(heatIndex) + "˚C\n");
 
-    // mqttClient.publish((sensor + "/temperature").c_str(), String(temperature).c_str(), true);
-    // mqttClient.publish((sensor + "/humidity").c_str(), String(humidity).c_str(), true);
-    // mqttClient.publish((sensor + "/heatindex").c_str(), String(heatIndex).c_str(), true);
-}
-void displayInterrupt()
-{
-    Serial.println("Interrupted, displaying current info");
-    // display.displayOn();
-    display.clear();
-    display.drawString(0, 10, "Raw:" + String(raw));
-    display.drawString(0, 20, "Level:" + String(level) + '%');
-    display.drawString(0, 30, "Humidity:" + String(humidity) + '%');
-    display.drawString(0, 40, "Temperature:" + String(temperature) + "˚C");
-    display.drawString(0, 50, "Heat Index:" + String(heatIndex) + "˚C");
-    display.display();
-    // display.displayOff();
+    mqttClient.publish((sensor + "/temperature").c_str(), String(temperature).c_str(), true);
+    mqttClient.publish((sensor + "/humidity").c_str(), String(humidity).c_str(), true);
+    mqttClient.publish((sensor + "/heatindex").c_str(), String(heatIndex).c_str(), true);
 }
